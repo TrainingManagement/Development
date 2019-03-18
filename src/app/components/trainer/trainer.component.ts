23e03ae7bddab8a3290e8a1bb5055d856a1de742
Sonone <Priyanka.Sonone@capco.com>
2019-03-18 09:28:06 +0530

    validation added

--- src/app/components/trainer/trainer.component.ts
+++ src/app/components/trainer/trainer.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injector } from "@angular/core";
+import { BaseApp } from "../../common/base-app";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import { CustomValidators } from "../../common/validations/CustomValidators";
 
 @Component({
-  selector: 'app-trainer',
-  templateUrl: './trainer.component.html',
-  styleUrls: ['./trainer.component.scss']
+  selector: "app-trainer",
+  templateUrl: "./trainer.component.html",
+  styleUrls: ["./trainer.component.scss"]
 })
-export class TrainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class TrainerComponent extends BaseApp implements OnInit {
+  trainerForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, injector: Injector) {
+    super(injector);
+    this.trainerForm = this.formBuilder.group({
+      trainingName: [
+        "",
+        [
+          Validators.required,
+          Validators.maxLength(70),
+          CustomValidators.cannotContainSpace
+        ]
+      ],
+      trainingType: [
+        "",
+        [Validators.required, CustomValidators.cannotContainSpace]
+      ]
+    });
   }
 
+  ngOnInit() {}
+
+  get trainingName() {
+    return this.trainerForm.controls["trainingName"];
+  }
+
+  get trainingType() {
+    return this.trainerForm.controls["trainingType"];
+  }
+
+  applyClass(control) {
+    console.log("check", control);
+    return control.touched ? (control.invalid ? "is-invalid" : "is-valid") : "";
+  }
 }
